acefa73fa2bf78fca15d1b0f87782a563ad7eab4
Vo Minh Thu <noteed@gmail.com>
2013-06-21 21:26:57 +0200

    Added test (missing implementation) for echo a >> echo b.

--- Intake.hs
+++ Intake.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TupleSections #-}
 module Intake where
 
@@ -12,25 +13,38 @@ import System.FilePath ((</>))
 
 ----------------------------------------------------------------------
 -- Types
+-- The trivial instances are there because the coverage reporting
+-- does not count the corresponding 'deriving' clauses.
 ----------------------------------------------------------------------
 
 newtype WorkflowIdPrefix = WorkflowIdPrefix String
-  deriving Show
+
+instance Show WorkflowIdPrefix where
+  show (WorkflowIdPrefix s) = s
 
 -- | Some random string to identify a workflow instance.
 newtype WorkflowId = WorkflowId String
-  deriving (Eq, Show)
+
+instance Show WorkflowId where
+  show (WorkflowId s) = s
+
+instance Eq WorkflowId where
+  (WorkflowId a) == (WorkflowId b) = a == b
 
 -- | Identify a workflow configuration, as registered with `intake define`.
 newtype WorkflowName = WorkflowName String
-  deriving (Eq, Show)
+
+instance Show WorkflowName where
+  show (WorkflowName s) = s
+
+instance Eq WorkflowName where
+  (WorkflowName a) == (WorkflowName b) = a == b
 
 data Workflow =
     Job String [String] -- ^ command and arguments
   | Sequence Workflow Workflow
   | Parallel Workflow Workflow
   | Retry Int Workflow
-  deriving Show
 
 -- | Same as `Workflow` but with additional data.
 data WorkflowState =
@@ -38,7 +52,20 @@ data WorkflowState =
   | SSequence WorkflowState WorkflowState
   | SParallel WorkflowState WorkflowState
   | SRetry Int Int WorkflowState -- ^ Maximum allowed attempts , already tried.
-  deriving (Eq, Show)
+
+instance Show WorkflowState where
+  show (SJob l cmd args s) = "#" ++ show l ++ " " ++ cmd ++ " " ++ show args ++ "(" ++ show s ++ ")"
+  show (SSequence a b) = "(" ++ show a ++ " >> " ++ show b ++ ")"
+  show (SParallel a b) = "(" ++ show a ++ " // " ++ show b ++ ")"
+  show (SRetry m n a) = show a ++ " : " ++ show n ++ "/" ++ show m
+
+instance Eq WorkflowState where
+  SJob i cmd args s == SJob i' cmd' args' s' = i == i' && cmd == cmd'
+   && args == args' && s == s'
+  SSequence a b == SSequence a' b' = a == a' && b == b'
+  SParallel a b == SSequence a' b' = a == a' && b == b'
+  SRetry m n a == SRetry m' n' a' = m == m' && n == n' && a == a'
+  _ == _ = False
 
 data JStatus = Waiting | Ready | Started | Completed
   deriving (Eq, Show, Read)
@@ -49,10 +76,28 @@ data WorkflowEnv = WorkflowEnv
   , envArguments :: [String]
   , envState :: WorkflowState
   }
-  deriving (Eq, Show)
+
+instance Show WorkflowEnv where
+  show WorkflowEnv{..} = unlines
+    [ "workflow: " ++ show envName
+    , "id: " ++ show envId
+    , "arguments: " ++ show envArguments
+    , "state: " ++ show envState
+    ]
+
+instance Eq WorkflowEnv where
+  a == b = envName a == envName b &&
+    envId a == envId b &&
+    envArguments a == envArguments b &&
+    envState a == envState b
 
 newtype Run = Run Int
-  deriving (Eq, Show)
+
+instance Show Run where
+  show (Run i) = "run #" ++ show i
+
+instance Eq Run where
+  (Run a) == (Run b) = a == b
 
 -- | Whole Workflow status.
 data WStatus = WInstanciated | WStarted [Int] | WCompleted
@@ -80,7 +125,7 @@ run name@(WorkflowName n) arguments = do
 start :: WorkflowId -> WorkflowEnv -> Run -> IO ()
 start (WorkflowId i') e r = do
   home <- getHomeDirectory
-  let (cmd, args) = extract e r
+  let (cmd, args) = maybe (error "No such job ID.") id $ extract e r
       Run l = r
       dir = home </> ".intake" </> i' </> show l
   putStrLn $ take 12 i' ++ "  Starting `" ++ cmd
@@ -155,14 +200,29 @@ labelWorkflow l w = case w of
   Sequence a b -> let (a', l') = labelWorkflow l a
                       (b', l'') = labelWorkflow l' b
                   in (SSequence a' b', l'')
+  Parallel a b -> let (a', l') = labelWorkflow l a
+                      (b', l'') = labelWorkflow l' b
+                  in (SParallel a' b', l'')
+  Retry n a -> let (a', l') = labelWorkflow l a
+               in (SRetry 0 n a', l')
 
 makeReady  :: Bool -> WorkflowState -> (WorkflowState, Bool)
 makeReady ready w = case w of
   SJob l c as Waiting -> (SJob l c as (if ready then Waiting else Ready), True)
+  SJob l c as Ready -> (SJob l c as Ready, False)
+  SJob l c as Started -> (SJob l c as Started, False)
+  SJob l c as Completed -> (SJob l c as Completed, False)
   SSequence a b -> let (a', ready') = makeReady ready a
                        (b', ready'') = makeReady ready' b
                    in (SSequence a' b', ready'')
+  SParallel a b -> let (a', ready') = makeReady ready a
+                       (b', ready'') = makeReady ready b
+                   in (SParallel a' b', ready' || ready'')
+  SRetry m n a -> let (a', ready') = makeReady ready a
+                  in (SRetry m n a', ready')
 
+-- | Find all `Ready` jobs, and start them (i.e. change their status to
+-- `Started` and return their ID as Run commands).
 step :: WorkflowEnv -> (WorkflowEnv, [Run])
 step e =
   let (s, rs) = step' $ envState e
@@ -172,18 +232,57 @@ step' :: WorkflowState -> (WorkflowState, [Run])
 step' s = do
   case s of
     SJob l cmd args Ready -> (SJob l cmd args Started, [Run l])
+    SSequence a b ->
+      if isCompleted a
+      then let (b', rs) = step' b
+           in (SSequence a b', rs)
+      else let (a', rs) = step' a
+           in (SSequence a' b, rs)
     job -> (job, [])
 
-extract :: WorkflowEnv -> Run -> (String, [String])
-extract e (Run l) = case envState e of
-  SJob l' cmd args _ | l' == l -> (cmd, args)
+isCompleted :: WorkflowState -> Bool
+isCompleted s = case s of
+  SJob _ _ _ Completed -> True
+  SSequence a b -> isCompleted a && isCompleted b
+  SParallel a b -> isCompleted a && isCompleted b
+  SRetry _ _ a -> isCompleted a
+  _ -> False
+
+extract :: WorkflowEnv -> Run -> Maybe (String, [String])
+extract e r = extract' (envState e) r
+
+extract' :: WorkflowState -> Run -> Maybe (String, [String])
+extract' s (Run l) = case s of
+  SJob l' cmd args _ | l' == l -> Just (cmd, args)
+  SSequence a b -> case extract' a (Run l) of
+    Nothing -> extract' b (Run l)
+    found -> found
 
 status :: WorkflowEnv -> WStatus
-status e = case envState e of
+status e = status' $ envState e
+
+status' :: WorkflowState -> WStatus
+status' s = case s of
   SJob _ _ _ Completed -> WCompleted
   SJob l _ _ Started -> WStarted [l]
+  SSequence a b -> case (status' a, status' b) of
+    (WCompleted, WCompleted) -> WCompleted
+    (WInstanciated, WInstanciated) -> WInstanciated
+    (WStarted as, WStarted bs) -> WStarted $ as ++ bs
+    (WStarted as, _) -> WStarted as
+    (_, WStarted bs) -> WStarted bs
+    (WCompleted, WInstanciated) -> WStarted []
+    (WInstanciated, WCompleted) -> WStarted []
   _ -> WInstanciated
 
 setStatus :: Int -> JStatus -> WorkflowEnv -> WorkflowEnv
-setStatus l s e = case envState e of
-  SJob l' cmd args _ | l == l' -> e { envState = SJob l cmd args s }
+setStatus l s e = e { envState = setStatus' l s (envState e) }
+
+setStatus' l st s = case s of
+  SJob l' cmd args st' | l == l' -> SJob l' cmd args st
+                       | otherwise -> SJob l' cmd args st'
+  SSequence a b -> setStatus' l st a `SSequence` setStatus' l st b
+
+complete l e = e { envState = fst $ makeReady False $ complete' l $ envState e }
+
+complete' l = setStatus' l Completed
--- bin/intake.hs
+++ bin/intake.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
 import System.Environment (getArgs)
-import Test.HUnit (assertEqual)
+import Test.HUnit (assertBool, assertEqual)
 import Test.Framework (defaultMain, Test)
 import Test.Framework.Providers.HUnit (testCase)
 
@@ -24,10 +24,15 @@ main = do
     -- "show" to improve coverage
     ["show"] -> do
       print echoA
+      print echoAB
+      print echoAB'
+      print echoRA
       print $ Run 0
+      print $ WorkflowIdPrefix "a"
       let jstatus :: [JStatus]
           jstatus = read "[Waiting, Ready, Started, Completed]"
       print $ filter (/= Ready) jstatus
+      print $ filter (/= WInstanciated) [WStarted [], WCompleted]
     [] -> do
       defaultMain tests
 
@@ -40,14 +45,29 @@ tests =
   [ testCase "echo a" $ do
       assertEqual "instanciated" WInstanciated (status echoA)
       assertEqual "started" (WStarted [0]) (status . fst $ step echoA)
+      assertBool "started'" $ echoA /= fst (step echoA)
       assertEqual "run 0" [Run 0] (snd $ step echoA)
       assertEqual "nothing to change" (fst $ step echoA) (fst. step . fst $ step echoA)
       assertEqual "nothing to run" [] (snd . step . fst $ step echoA)
-      assertEqual "completed" (WCompleted) (status . setStatus 0 Completed . fst $ step echoA)
-      assertEqual "nothing to run" [] (snd . step . setStatus 0 Completed . fst $ step echoA)
-  , testCase "echo a ; echo b" $ do
-      assertEqual "instanciated" WInstanciated (status echoAB)
-      -- TODO assertEqual "started" (WStarted [0]) (status . fst $ step echoAB)
+      assertEqual "completed" (WCompleted) (status . complete 0 . fst $ step echoA)
+      assertEqual "nothing to run" [] (snd . step . complete 0 . fst $ step echoA)
+
+  , testCase "echo a >> echo b" $ do
+      assertEqual "instanciated" (wrap' $ SJob 0 "echo" ["a"] Ready `SSequence` SJob 1 "echo" ["b"] Waiting) (echoAB)
+      assertEqual "instanciated'" WInstanciated (status echoAB)
+      assertEqual "run 0" [Run 0] (snd $ step echoAB)
+      assertEqual "started" (WStarted [0]) (status . fst $ step echoAB)
+      assertEqual "still started" (WStarted []) (status . complete 0 . fst $ step echoAB)
+      assertEqual "still started"
+        (wrap' $ SJob 0 "echo" ["a"] Completed `SSequence` SJob 1 "echo" ["b"] Ready)
+        (complete 0 . fst $ step echoAB)
+      assertEqual "still started"
+        (wrap' $ SJob 0 "echo" ["a"] Completed `SSequence` SJob 1 "echo" ["b"] Started)
+        (fst . step . complete 0 . fst $ step echoAB)
+      assertEqual "run 1" [Run 1] (snd . step . complete 0 . fst $ step echoAB)
+      assertEqual "still started" (WStarted [1]) (status . fst . step . complete 0 . fst $ step echoAB)
+      assertEqual "completed" (WCompleted) (status . complete 1 . fst . step . complete 0 . fst $ step echoAB)
+      assertEqual "nothing to run" [] (snd . step . complete 1 . fst . step . complete 0 . fst $ step echoAB)
   ]
 
 echoA :: WorkflowEnv
@@ -56,11 +76,20 @@ echoA = wrap $ echo "a"
 echoAB :: WorkflowEnv
 echoAB = wrap $ echo "a" `Sequence` echo "b"
 
+echoAB' :: WorkflowEnv
+echoAB' = wrap $ echo "a" `Parallel` echo "b"
+
+echoRA :: WorkflowEnv
+echoRA = wrap $ Retry 2 $ echo "a"
+
 echo :: String -> Workflow
 echo s = Job "echo" $ words s
 
 wrap :: Workflow -> WorkflowEnv
-wrap = WorkflowEnv (WorkflowName "test") (WorkflowId "_") [] . initializeWorkflow
+wrap = wrap' . initializeWorkflow
+
+wrap' :: WorkflowState -> WorkflowEnv
+wrap' = WorkflowEnv (WorkflowName "test") (WorkflowId "_") []
 
 -- property: if `step` issues some Run l, the state of the Job l is Started.
 
