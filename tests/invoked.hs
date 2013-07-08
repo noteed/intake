782e85b9b9f6adfc12dc2b2da6a9815efcd0a10e
Vo Minh Thu <noteed@gmail.com>
2013-07-08 20:25:40 +0200

    invoked: allow to ignore stdout, stderr, or exit code.

--- tests/invoked.hs
+++ tests/invoked.hs
@@ -4,6 +4,7 @@
 -- produce. That's all Folks!
 module Main (main) where
 
+import Data.Maybe (fromJust)
 import System.Environment (getArgs)
 import System.Exit (exitWith, ExitCode(..))
 import System.IO (hPutStrLn, stderr)
@@ -24,28 +25,39 @@ data Invoke = Invoke
   { inCommand :: String -- ^ The command to execute.
   , inArguments :: [String] -- ^ Arguments for the command.
   , inStdin :: String -- ^ Standard input to provide to the command.
-  , inStderr :: String -- ^ Expected standard error.
-  , inStdout :: String -- ^ Expected standard output.
-  , inExitCode :: ExitCode -- ^ Expected exit code.
+  , inStderr :: Maybe String -- ^ Expected standard error.
+  , inStdout :: Maybe String -- ^ Expected standard output.
+  , inExitCode :: Maybe ExitCode -- ^ Expected exit code.
   }
 
-readInvoke :: String -> IO Invoke
+defaultInvoke :: Invoke
+defaultInvoke = Invoke "true" [] "" (Just "") (Just "") (Just ExitSuccess)
+
+readInvoke :: String -> IO [Invoke]
 readInvoke filename = do
   content <- readFile filename
   let content' = lines content
       command : arguments = words' $ head content'
       expected = unlines $ tail content'
       expected' = if last content /= '\n' then init expected else expected
-  return $ Invoke command arguments "" "" expected' ExitSuccess
+  return [Invoke command arguments "" (Just "") (Just expected') (Just ExitSuccess)]
 
-runInvoke :: Invoke -> IO ExitCode
-runInvoke Invoke{..} = do
+runInvoke :: [Invoke] -> IO ExitCode
+runInvoke [] = return ExitSuccess
+runInvoke (i : is) = do
+  r <- runInvoke' i
+  case r of
+    ExitSuccess -> runInvoke is
+    _ -> return r
+
+runInvoke' :: Invoke -> IO ExitCode
+runInvoke' Invoke{..} = do
   (code, out, err) <- readProcessWithExitCode inCommand inArguments inStdin
-  if code /= inExitCode
+  if maybe False (code /=) inExitCode
     then unexpectedExitCode
-    else if err /= inStderr
+    else if maybe False (err /=) inStderr
          then unexpectedStderr err
-         else if out /= inStdout
+         else if maybe False (out /=) inStdout
               then unexpectedStdout out
               else return ExitSuccess
   where
@@ -58,14 +70,14 @@ runInvoke Invoke{..} = do
     hPutStrLn stderr "The command stderr is:"
     hPutStrLn stderr err'
     hPutStrLn stderr "The expected stderr is:"
-    hPutStrLn stderr inStderr
+    hPutStrLn stderr $ fromJust inStderr
     return $ ExitFailure 1
   unexpectedStdout out' = do
     hPutStrLn stderr "The command stdout is not the expected output."
     hPutStrLn stderr "The command stdout is:"
     hPutStrLn stderr out'
     hPutStrLn stderr "The expected output is:"
-    hPutStrLn stderr inStdout
+    hPutStrLn stderr $ fromJust inStdout
     return $ ExitFailure 1
 
 -- | Same as the standard `words` function but try to respect quoted strings.
