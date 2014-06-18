87e7fe60f36dd7278196c5bb8c1556b297f8da99
Vo Minh Thu <noteed@gmail.com>
2014-06-18 11:39:09 +0200

    Change a bit dependencies versions.

--- tests/intake-run-00.hs
+++ tests/intake-run-00.hs
@@ -2,22 +2,28 @@ import Intake.Core
 import Intake.Job
 import System.Exit
 main = defaultMain'
-  [ defaultJob
+  [
+  -- Run `intake run a` and capture its output as SHORT_ID and ID.
+  defaultJob
   { jobCommand = "./dist/build/intake/intake"
   , jobArguments = words "run a"
   , jobStderr = Just ""
   , jobStdout = Just "{{SHORT_ID}}  {{ID}}\n"
   }
+  -- Wait a bit.
   , defaultJob
   { jobCommand = "sleep"
   , jobArguments = words "0.1"
   }
+  -- Run `intake status $SHORT_ID` with the captured SHORT_ID.
   , defaultJob
   { jobCommand = "./dist/build/intake/intake"
   , jobArguments = words "status {{$SHORT_ID}}"
   , jobStderr = Just ""
   , jobStdout = Just "WCompleted\n"
   }
+  -- Run `intake show $SHORT_ID` with the captured SHORT_ID.
+  -- This checks the stdout by comparing the captured ID.
   , defaultJob
   { jobCommand = "./dist/build/intake/intake"
   , jobArguments = words "show {{$SHORT_ID}}"
